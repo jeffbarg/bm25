7d17707859f3f72d88f3a7ecc1c2196547009a90
Jeff Barg <jeffbarg94@gmail.com>
2024-06-24 12:23:20 -0400

    Add some more variables to the config and fix tests

--- src/Bm25Search.ts
+++ src/Bm25Search.ts
@@ -1,15 +1,32 @@
+export namespace Bm25Search {
+  export type ImplementationType = "lucene"; // More types can be added here
+  export interface Config {
+    method: ImplementationType;
+  }
+}
+
 export class Bm25Search {
+  // Constants
+  private _k1 = 1.5;
+  private _b = 0.75;
+  private _delta = 0.5;
+
+  // Configuration
+  private _method = "lucene";
+
+  // Raw data
   private _documents: Array<string>;
 
-  constructor() {
+  constructor(config: Bm25Search.Config = { method: "lucene" }) {
     this._documents = [];
+    this._method = config.method;
   }
 
   addDocuments(documents: string[]): void {
-    throw new Error("Not implemented");
+    this._documents.push(...documents);
   }
 
-  search(query: string): string {
+  search(query: string): string[] {
     throw new Error("Not implemented");
   }
 }
--- tests/Bm25Search.test.ts
+++ tests/Bm25Search.test.ts
@@ -2,7 +2,13 @@ import { Bm25Search } from "../src/index";
 
 describe("Bm25Search module", () => {
   test("constructor should not throw an error", () => {
-    const search = new Bm25Search();
-    expect(search).toBeInstanceOf(Bm25Search);
+    const bm25 = new Bm25Search();
+    expect(bm25).toBeInstanceOf(Bm25Search);
+  });
+
+  test("addDocuments should throw an error", () => {
+    const bm25 = new Bm25Search();
+    expect(() => bm25.addDocuments([])).not.toThrow();
+    expect(() => bm25.addDocuments(["document 1", "document 2", "document 3"])).not.toThrow();
   });
 });
