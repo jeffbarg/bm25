7f2170367a5befc76332307b9b9bce8b3a38122d
Jeff Barg <jeffbarg94@gmail.com>
2024-06-27 07:49:25 -0400

    remove unecessary imports

--- example/src/components/DemoTable.tsx
+++ example/src/components/DemoTable.tsx
@@ -2,20 +2,6 @@
 
 import * as React from "react";
 import { Bm25Search } from "@acmexyz/bm25-search";
-
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
